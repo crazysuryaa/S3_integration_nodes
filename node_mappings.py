63e67b70ce9269f77e7840ea269412b05d517f3e
crazysuryaa <suryakirancherupally@gmail.com>
2025-08-31 03:31:19 -0700

    audio mappings

--- node_mappings.py
+++ node_mappings.py
@@ -8,16 +8,19 @@ try:
     from .s3_config_node import S3ConfigNode
     from .s3_image_node import S3ImageNode
     from .s3_video_node import S3VideoNode
+    from .s3_audio_node import S3AudioNode
 except ImportError:
     from s3_config_node import S3ConfigNode
     from s3_image_node import S3ImageNode
     from s3_video_node import S3VideoNode
+    from s3_audio_node import S3AudioNode
 
 # Node class mappings for ComfyUI
 NODE_CLASS_MAPPINGS = {
     "S3ConfigNode": S3ConfigNode,
     "S3ImageNode": S3ImageNode,
     "S3VideoNode": S3VideoNode,
+    "S3AudioNode": S3AudioNode,
 }
 
 # Display name mappings for ComfyUI interface
@@ -25,6 +28,7 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "S3ConfigNode": "S3 Configuration",
     "S3ImageNode": "S3 Image Operations",
     "S3VideoNode": "S3 Video Operations",
+    "S3AudioNode": "S3 Audio Operations",
 }
 
 # Export for ComfyUI
